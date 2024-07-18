5605c65324e1f1e09730a6c3ede4f363379bfbc0
Martin Bångens <noisecode3@gmail.com>
2024-07-18 17:13:12 +0200

    add convert.sh

---
+++ utils/convert.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+for file in *.wma; do ffmpeg -i "$file" -c:a libvorbis -q:a 4 "${file%.*}.ogg"; done
+i=12
+for file in *.ogg; do
+    mv "$file" "Track$(printf %02d $i).ogg"
+    ((i++))
+done
