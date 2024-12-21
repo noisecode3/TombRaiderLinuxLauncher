dbe5a72ad6b685fb816a0406283bac08b8c60c08
Martin Bångens <noisecode3@gmail.com>
2024-12-21 14:54:09 +0100

    handle libs/miniz warning and ignore changes when setting it up

--- setup_nvim_lsp.sh
+++ setup_nvim_lsp.sh
@@ -31,3 +31,5 @@ cd build || exit 1
 cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug "$@" ..
 cd ..
 ln -s build/compile_commands.json compile_commands.json
+
+git config submodule.libs/miniz.ignore all
