c7801f2b8e898f323b18e20d6b390174c6e009d9
Martin Bångens <noisecode3@gmail.com>
2025-06-15 23:17:11 +0200

    rebase Into The Realm Of Eternal Darkness

---
+++ fixes/3557/TRX_setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd "$(dirname "$0")" || exit 1
+rm -fr TRX
+md5sum="475b292dee44e978bda1567459bfaabd"
+md5sum_file=$(md5sum BlackWolfTR-IntoTheRealmOfEternalDarkness.zip | cut -d' ' -f1)
+
+if [[ $md5sum == "$md5sum_file" ]]
+    then
+        echo "sum OK"
+    else
+        rm -f BlackWolfTR-IntoTheRealmOfEternalDarkness.zip
+        wget --no-check-certificate https://www.trle.net/levels/levels/2023/0723/BlackWolfTR-IntoTheRealmOfEternalDarkness.zip
+        md5sum_file=$(md5sum BlackWolfTR-IntoTheRealmOfEternalDarkness.zip | cut -d' ' -f1)
+        if [[ $md5sum == "$md5sum_file" ]]
+            then
+                echo "sum OK"
+            else
+                rm -f BlackWolfTR-IntoTheRealmOfEternalDarkness.zip
+                exit 1
+        fi
+fi
+   git clone --recurse-submodules https://github.com/LostArtefacts/TRX
+   cd TRX || exit 1
+   git checkout c1a9933
+   patch -p1 < ../IntoTheRealmOfEternalDarkness.patch
+   just tr1-package-linux release
--- setup_nvim_lsp.sh
+++ setup_nvim_lsp.sh
@@ -7,7 +7,7 @@ mkdir build
 cd build || exit 1
 
 cmake \
-    -DCMAKE_INSTALL_PREFIX=/home/$USER/.local \
+    -DCMAKE_INSTALL_PREFIX=/home/"$USER"/.local \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
     -DNO_DATABASE=ON \
     -DCMAKE_BUILD_TYPE=Debug \
