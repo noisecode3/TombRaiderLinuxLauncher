a2c924f88651a8aaf471cc26dfb170eb9d5a733b
Martin Bångens <noisecode3@gmail.com>
2025-06-16 19:27:57 +0200

    update IntoTheRealmOfEternalDarkness

--- fixes/3557/TRX_setup.sh
+++ fixes/3557/TRX_setup.sh
@@ -24,3 +24,8 @@ fi
    git checkout c1a9933
    patch -p1 < ../IntoTheRealmOfEternalDarkness.patch
    just tr1-package-linux release
+   cd ..
+   mkdir IntoTheRealmOfEternalDarkness
+   cd IntoTheRealmOfEternalDarkness || exit 1
+   unzip ../BlackWolfTR-IntoTheRealmOfEternalDarkness.zip
+   unzip ../TRX/TR1X-4.11.2-dirty-Linux.zip
