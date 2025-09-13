adb96772a2cd433cda3a1dc9b93b260a530ef3c8
Martin Bångens <noisecode3@gmail.com>
2025-09-13 20:14:05 +0200

    update pinned key

--- database/https.py
+++ database/https.py
@@ -208,7 +208,7 @@ class RequestHandler:
                     temp_cert_path = self.set_leaf(curl)
                     curl.setopt(pycurl.SSL_VERIFYPEER, 1)
                     curl.setopt(pycurl.SSL_VERIFYHOST, 2)
-                    pinned_key = "sha256//87UOkLPD3rxuss0AWbDbdDI0NPZvT6xpQHkQGxHpWE8="
+                    pinned_key = "sha256//6Lgo+X8VJS/jyRHfOJwsTdYQN2iTCCaj4tO3+m0q76U="
                     curl.setopt(pycurl.PINNEDPUBLICKEY, pinned_key)
 
                 headers_list = [
